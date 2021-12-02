395d2cb1bca3ea52146bb2b83f6ccde1aa748bd8
Bloectasy <123davidacevski@gmail.com>
2021-12-03 00:55:26 +0100

    refactor: Refactored commands and listeners.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { Valeriyya } from "#lib/ValeriyyaClient";
+import { Valeriyya } from "./lib/ValeriyyaClient"
 
 const client: Valeriyya = new Valeriyya();
 
-client.login();
+client.login("OTA5NzkxNDU0MDQwMzAxNTY4.YZJbUQ.c8PIUM_EftouBg9KKV9bDG6IWCY");
