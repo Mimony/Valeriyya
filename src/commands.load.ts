7a46e3e04afbb7f667b388f81ab687dc13e7160c
Bloectasy <123davidacevski@gmail.com>
2021-12-04 20:15:11 +0100

    feat: ApplicationCommand Loaders.

---
+++ src/commands.load.ts
@@ -0,0 +1,20 @@
+import client from "./index";
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+
+const clientID = "909791454040301568";
+
+const rest = new REST({ version: "9" }).setToken("OTA5NzkxNDU0MDQwMzAxNTY4.YZJbUQ.c8PIUM_EftouBg9KKV9bDG6IWCY");
+
+(async () => {
+    try {
+        client.logger.print("Loading Application Commands....");
+
+        await rest.put(Routes.applicationCommands(clientID), { body: client.commands.map(c => c.data) });
+
+        client.logger.print("Finished Loading Application Commands.");
+
+    } catch(err: any) {
+        client.logger.error(err);
+    }
+})()
--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ import { Valeriyya } from "./lib/valeriyya.client"
 const client: Valeriyya = new Valeriyya();
 
 client.login("OTA5NzkxNDU0MDQwMzAxNTY4.YZJbUQ.c8PIUM_EftouBg9KKV9bDG6IWCY");
+
+export default client;
