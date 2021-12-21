82d04d8aed76415d5f88dbe0103f16e6a2dd2c26
Bloectasy <47886919+Bloectasy@users.noreply.github.com>
2021-12-21 19:31:09 +0100

    Stop the bot

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import { Valeriyya } from "./lib/valeriyya.client"
 
 const client: Valeriyya = new Valeriyya();
 
-client.start("OTA5NzkxNDU0MDQwMzAxNTY4.YZJbUQ.c8PIUM_EftouBg9KKV9bDG6IWCY");
+//client.start("OTA5NzkxNDU0MDQwMzAxNTY4.YZJbUQ.c8PIUM_EftouBg9KKV9bDG6IWCY");
