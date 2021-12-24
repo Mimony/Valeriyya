97e6489c06e85be2d0893136fe3b69c93d7bf776
Bloectasy <123davidacevski@gmail.com>
2021-12-25 00:09:03 +0100

    fix: Settings to traditional subcommands and refactor: package.json scripts

--- src/commands/settings/settings.ts
+++ src/commands/settings/settings.ts
@@ -1,18 +1,81 @@
-import { GuildMember, Message, MessageActionRow, MessageSelectMenu } from "discord.js";
-import { defineCommand, type ICommandInteraction } from "../../lib/util/valeriyya.types";
+import type { GuildMember, Role, TextBasedChannels } from "discord.js";
+import { defineCommand, type ICommandInteraction, OptionTypes } from "../../lib/util/valeriyya.types";
 import { ValeriyyaEmbed } from "../../lib/util/valeriyya.embed";
 
 export default defineCommand({
     data: {
         name: "settings",
-        description: "Changes the settings in this guild."
+        description: "Changes the settings in this guild.",
+        options: [
+            {
+                name: "channel",
+                description: "Select a channel setting.",
+                type: OptionTypes.SUB_COMMAND,
+                options: [
+                    {
+                        name: "type",
+                        description: "Choose what the channel is for.",
+                        type: OptionTypes.STRING,
+                        choices: [
+                            {
+                                name: "logs",
+                                value: "logs"
+                            },
+                            {
+                                name: "welcome",
+                                value: "welcome"
+                            }
+                        ],
+                        required: true,
+                    },
+                    {
+                        name: "channel",
+                        description: "The channel that will be used for the previous type.",
+                        type: OptionTypes.CHANNEL,
+                        channelTypes: ["GUILD_TEXT", "GUILD_NEWS"],
+                        required: true
+                    }
+                ]
+            },
+            {
+                name: "role",
+                description: "Select a role setting.",
+                type: OptionTypes.SUB_COMMAND,
+                options: [
+                    {
+                        name: "type",
+                        description: "Choose what the role is for.",
+                        type: OptionTypes.STRING,
+                        choices: [
+                            {
+                                name: "staff",
+                                value: "staff"
+                            },
+                            {
+                                name: "mute",
+                                value: "mute"
+                            }
+                        ],
+                        required: true,
+                    },
+                    {
+                        name: "role",
+                        description: "The role that will be used for the previous type.",
+                        type: OptionTypes.ROLE,
+                        required: true
+                    }
+                ]
+            }
+        ]
     },
     execute: async (int: ICommandInteraction) => {
-        await int.deferReply({ ephemeral: true });
-        const member = int.member;
+        const member = int.member as GuildMember;
         const db = await int.client.db(int.guild!);
-
-        if (!(member instanceof GuildMember)) return;
+        const cmd = int.options.getSubcommand();
+        const channel_type = int.options.getString("type") as "logs" | "welcome";
+        const role_type = int.options.getString("type") as "staff" | "mute";
+        const role = int.options.getRole("role") as Role;
+        const channel = int.options.getChannel("channel") as TextBasedChannels;
 
         if (!member.permissions.has("MANAGE_GUILD", true)) return {
             embeds: [
@@ -21,190 +84,23 @@ export default defineCommand({
                     .setDescription("You are missing the `MANAGE_GUILD` permission")
             ]
         }
-        let row = new MessageActionRow()
 
-            let settingsMenu = new MessageSelectMenu()
-                .setCustomId("settings")
-                .setPlaceholder("Select what setting to change.")
-                .addOptions(
-                    {
-                        value: "staff",
-                        description: "Choose a staff role.",
-                        label: "staff"
-                    },
-                    {
-                        value: "mute",
-                        description: "Choose a mute role.",
-                        label: "mute"
-                    },
-                    {
-                        value: "welcome",
-                        description: "Choose a welcome channel.",
-                        label: "welcome"
-                    },
-                    {
-                        value: "logs",
-                        description: "Choose a logs channel.",
-                        label: "logs"
-                    }
-                )
-            row.setComponents(settingsMenu)
-
-        let channelsMenu1 = new MessageSelectMenu()
-            .setCustomId("settings.channels.welcome")
-            .setPlaceholder("Provide a channel that will be used for welcome messages.")
-        int.guild!.channels.cache.filter(c => {
-            if (member.permissions.has("ADMINISTRATOR")) return c.type === "GUILD_TEXT";
-            return c.type === "GUILD_TEXT" && c.permissionsFor(member, true).has("VIEW_CHANNEL");
-        }).each(c => {
-            // TODO Divide Channels in more menus
-            channelsMenu1.addOptions(
-                {
-                    value: c.id,
-                    description: `Select a channel to set it as a welcome channel.`,
-                    label: c.name
-                }
-            )
-        })
-        let channelsMenu2 = new MessageSelectMenu()
-            .setCustomId("settings.channels.logs")
-            .setPlaceholder("Provide a channel that will be used for logs.")
-
-        int.guild!.channels.cache.filter(c => {
-            if (member.permissions.has("ADMINISTRATOR")) return c.type === "GUILD_TEXT";
-            return c.type === "GUILD_TEXT" && c.permissionsFor(member, true).has("VIEW_CHANNEL");
-        }).each(c => {
-            // TODO Divide Channels in more menus
-            channelsMenu2.addOptions(
-                {
-                    value: c.id,
-                    description: `Select a channel to set it as a logs channel.`,
-                    label: c.name
-                }
-            )
-        })
-
-        let rolesMenu1 = new MessageSelectMenu()
-            .setCustomId("settings.roles.staff")
-            .setPlaceholder("Provide a role that will be used as a staff role.")
-
-        int.guild!.roles.cache.filter(r => r.id !== r.guild.id).each(r => {
-            // TODO Divide Roles in more menus
-            rolesMenu1.addOptions(
-                {
-                    value: r.id,
-                    description: `Select a role to set it as a staff role.`,
-                    label: r.name
-                }
-            )
-        })
-
-
-        let rolesMenu2 = new MessageSelectMenu()
-            .setCustomId("settings.roles.mute")
-            .setPlaceholder("Provide a role that will be used as a mute role.")
-
-        int.guild!.roles.cache.filter(r => r.id !== r.guild.id).each(r => {
-            // TODO Divide Roles in more menus
-            rolesMenu2.addOptions(
-                {
-                    value: r.id,
-                    description: `Select a role to set it as a mute role.`,
-                    label: r.name
-                }
-            )
-        })
-
-        const settings_menu = await int.followUp({
-            content: `Select what role/channel to change.`,
-            components: [row],
-            fetchReply: true
-        });
-
-        let menu;
-
-        try {
-            const type_collector = await (settings_menu as Message).awaitMessageComponent({
-                time: 30000,
-                componentType: "SELECT_MENU"
-            });
-
-            if (type_collector.values[0] === "welcome") {
-                menu = await type_collector.update({
-                    content: `Select a ${type_collector.values[0]} channel`,
-                    components: [row.setComponents(channelsMenu1)],
-                    fetchReply: true
-                })
-            } else if (type_collector.values[0] === "logs") {
-                menu = await type_collector.update({
-                    content: `Select a ${type_collector.values[0]} channel`,
-                    components: [row.setComponents(channelsMenu2)],
-                    fetchReply: true
-                })
+        if (cmd === "channel") {
+            db.channels[channel_type] = channel.id;
+            db.save();
+            return {
+                content: `The ${channel_type} channel has been updated to ${channel}.`,
+                ephemeral: true,
             }
-            if (type_collector.values[0] === "staff") {
-                menu = await type_collector.update({
-                    content: `Select a ${type_collector.values[0]} role.`,
-                    components: [row.setComponents(rolesMenu1)],
-                    fetchReply: true
-                })
-            } else if (type_collector.values[0] === "mute") {
-                menu = await type_collector.update({
-                    content: `Select a ${type_collector.values[0]} role.`,
-                    components: [row.setComponents(rolesMenu2)],
-                    fetchReply: true
-                })
+        } else if (cmd === "role") {
+            db.roles[role_type] = role.id;
+            db.save();
+            return {
+                content: `The ${role_type} role has been updated to ${role}.`,
+                ephemeral: true
             }
-        } catch (e: any) {
-            int.client.logger.error`The type collector has failed ${e.message}`
-            int.editReply({
-                content: `Selection has been canceled.`,
-                components: []
-            })
         }
 
-        try {
-            const collector = await (menu as Message).awaitMessageComponent({
-                time: 30000,
-                componentType: "SELECT_MENU"
-            })
-
-            if (collector.customId === "settings.roles.staff") {
-                db.roles.staff = collector.values[0];
-                db.save();
-                collector.update({
-                    content: `The staff role has been updated to ${int.guild!.roles.resolve(collector.values[0])}`,
-                    components: []
-                })
-            } else if (collector.customId === "settings.channels.logs") {
-                db.channels.logs = collector.values[0];
-                db.save();
-                collector.update({
-                    content: `The logs channel has been updated to ${int.guild!.channels.resolve(collector.values[0])}`,
-                    components: []
-                })
-            } else if (collector.customId === "settings.roles.mute") {
-                db.roles.mute = collector.values[0];
-                db.save();
-                collector.update({
-                    content: `The mute role has been updated to ${int.guild!.roles.resolve(collector.values[0])}`,
-                    components: []
-                })
-            } else if (collector.customId === "settings.channels.welcome") {
-                db.channels.welcome = collector.values[0];
-                db.save();
-                collector.update({
-                    content: `The welcome channel has been updated to ${int.guild!.channels.resolve(collector.values[0])}`,
-                    components: []
-                })
-            }
-        } catch (e: any) {
-            int.client.logger.error`The role | channel collector has failed ${e.message}`
-            int.editReply({
-                content: `Selection has been canceled.`,
-                components: []
-            })
-        }
         return;
     }
 })
