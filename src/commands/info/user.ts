23e27515dac91f42051a019d936924d131f1a9f7
Bloectasy <123davidacevski@gmail.com>
2022-01-10 03:34:29 +0100

    Removed moderation history from the user command

--- src/commands/info/user.ts
+++ src/commands/info/user.ts
@@ -3,39 +3,33 @@ import { ValeriyyaEmbed } from "../../lib/util/valeriyya.embed";
 import { defineCommand, ICommandInteraction, OptionTypes } from "../../lib/util/valeriyya.types";
 
 export default defineCommand({
-    data: {
+  data: {
+    name: "user",
+    description: "Gets the information about a user.",
+    options: [
+      {
         name: "user",
-        description: "Gets the information about a user.",
-        options: [
-            {
-                name: "user",
-                description: "Gets the information about this user.",
-                type: OptionTypes.USER,
-            },
-        ],
-    },
-    chat: async (int: ICommandInteraction) => {
-        const member = (int.options.getMember("user") || int.member) as GuildMember;
-        const db = await int.client.db(int.guild!);
-        const history = (await db.getUserHistory(member.id))!
-        const { user } = member!;
+        description: "Gets the information about this user.",
+        type: OptionTypes.USER,
+      },
+    ],
+  },
+  chat: async (int: ICommandInteraction) => {
+    const member = (int.options.getMember("user") || int.member) as GuildMember;
+    const { user } = member!;
 
-        const embed = new ValeriyyaEmbed()
-            .setAuthor({name: `${user.username}`, iconURL: user.displayAvatarURL({dynamic: true}) })
-            .setDescription(`
+    const embed = new ValeriyyaEmbed().setAuthor({ name: `${user.username} (${user.id})`, iconURL: user.displayAvatarURL({ dynamic: true }) }).setDescription(`
     User Created at: ${timeFormat(user.createdAt)} ${user.bot ? "(User is a bot)" : ""}
     Member Joined At: ${timeFormat(member.joinedAt)}
-    ID: ${user.id}
-    Moderation History: \`\`\`bans: ${history.ban}\nkicks: ${history.kick}\nmutes: ${history.mute}\`\`\`
-    `)
+    `);
 
-        return {
-            embeds: [embed],
-        };
-    },
+    return {
+      embeds: [embed],
+    };
+  },
 });
 
 function timeFormat(date: Date | null): string {
-    const time = Math.floor(date!.getTime());
-    return `<t:${time}:d>`;
+  const time = Math.floor(date!.getTime());
+  return `<t:${time}:d>`;
 }
