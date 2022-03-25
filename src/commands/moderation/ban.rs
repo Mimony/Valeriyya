ef9756c42e35f79085a4a93861b01dbc1f9297fb
Bloectasy <123davidacevski@gmail.com>
2022-03-25 22:58:02 +0100

    finish: Ban Command

--- src/commands/moderation/ban.rs
+++ src/commands/moderation/ban.rs
@@ -1,6 +1,7 @@
+use poise::serenity_prelude::{UserId};
+
 use crate::{serenity, Context, Error, member_managable};
 
-use self::serenity::UserId;
 
 #[poise::command(slash_command, category = "Moderation")]
 pub async fn ban(ctx: Context<'_>, 
@@ -12,24 +13,42 @@ pub async fn ban(ctx: Context<'_>,
     let reason = reason_option.unwrap_or_else(|| String::from("Default reason"));
 
     if let Some(m) = &member {
-        println!("{}", m.roles.len());
-        if member_managable(ctx, m).await {
-            println!("{}", m.roles.len());
-            // m.ban_with_reason(ctx.discord(), 7, &reason).await;
-            // ctx.send(|s| {
-            //    s.content(format!("{} has been banned", m.user.name));
-            //    s.ephemeral(true)
-            // });
+
+        if member_managable(ctx, m).await {            
+            m.ban_with_reason(ctx.discord(), 7, &reason).await;
+            ctx.send(|s| {
+                s.embed(|e| {
+                    e.author(|a| {
+                        a.name(format!("{} ({})", m.user.tag(), m.user.id));
+                        a.icon_url(ctx.author().avatar_url().unwrap_or_else(|| String::from("")))
+                    });
+                    e.thumbnail(ctx.guild().unwrap().icon.unwrap_or_else(|| String::from("")));
+                    e.description(format!("{} has been banned from {}", m.user.tag(), ctx.guild().unwrap().name))
+                });
+                s.ephemeral(true)
+            }).await;
         };
     }  
     if let Some(m_id) = &member_id {
-        println!("yes3");
+        if ctx.guild().unwrap().bans(&ctx.discord().http).await?.iter().any(|ban| ban.user.id == *m_id) {
+            ctx.send(|int| {
+                int.content("This member is already banned from this guild.");
+                int.ephemeral(true)
+            }).await;
+        }
         let user_id = UserId(*m_id);
-        ctx.guild().unwrap().ban_with_reason(ctx.discord(), user_id, 7, &reason);
+        ctx.guild().unwrap().ban_with_reason(ctx.discord(), user_id, 7, &reason).await;
         ctx.send(|s| {
-            s.content(format!("User with the id {} has been banned", user_id));
+            s.embed(|e| {
+                e.author(|a| {
+                    a.name(format!("{} ({})", "Unknown Tag", m_id));
+                    a.icon_url(ctx.author().avatar_url().unwrap_or_else(|| String::from("")))
+                });
+                e.thumbnail(ctx.guild().unwrap().icon.unwrap_or_else(|| String::from("")));
+                e.description(format!("{} has been banned from {}", m_id, ctx.guild().unwrap().name))
+            });
             s.ephemeral(true)
-         });
+         }).await;
     }
 
     Ok(())
